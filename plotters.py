720a7e0e574bf7caa78d211c4c8175b7aa0f23fa
Rachel Lee McClure <rlmcclure@wisc.edu>
2024-04-02 12:22:08 -0500

    add simovertime plotter

--- plotters.py
+++ plotters.py
@@ -258,3 +258,58 @@ def wbkg(fs=None,rtax=0,alph=1,fc='white'):
         return(f,ax)
     else:
         return f
+
+
+def simovert(frame,simstr='',xylim = 20,xycen=0,zlim=3,zcen=0,sv=1,alph=.7,lw=.5,c='k',htch=None,ls='--',cmapp='magma_r',physmult=1,pustr='',binsn=2000):
+    # frame = {}
+    try:
+        tind = np.unique(frame['t'])
+    except KeyError:
+        tind = 0
+    f = wbkg((6,8))
+    fig_mos = """
+    AA
+    BB
+    BB
+    """
+    ax = f.subplot_mosaic(fig_mos)#,subplot_kw={'sharex':1,'sharey':1})
+
+    x='x';y='z'
+    _,bbx,bby,_=ax['A'].hist2d(frame[x],frame[y],bins=np.linspace((-1*zlim-zcen)*physmult,(zlim-zcen)*physmult,binsn),cmin=1,alpha=1,norm=mpl.colors.LogNorm(),cmap=cmapp)
+    
+    ax['A'].set_xlim((-1*xylim-xycen)*physmult,(xylim-xycen)*physmult)
+    ax['A'].set_ylim((-1*zlim-zcen)*physmult,(zlim-zcen)*physmult)
+    ax['A'].set_ylabel(y+pustr)
+    # ax['A'].set_xlabel(x+' ['+SIM.spatialunitlabel+']')
+    ax['A'].grid(lw=.5,c='grey',alpha=.5)
+    # ax['A'].legend(prop={'size':10},loc=3)
+
+
+    x='x';y='y'
+    _,bbx,bby,_=ax['B'].hist2d(frame[x],frame[y],bins=np.linspace((-1*zlim-zcen)*physmult,(zlim-zcen)*physmult,binsn),cmin=1,alpha=1,norm=mpl.colors.LogNorm(),cmap=cmapp)
+    # px = bbx[(bbx>SIM.xinnerlim*physmult) & (bbx<barlims[tind]['barmax']*physmult)]
+    pl = 0 
+    # pu = SIM.youtlim*physmult
+    # ax['B'].fill_between(px,-1.0*pu,pu,fc='None',hatch=htch,alpha=alph,edgecolor=c,lw=lw,ls=ls)#,label='B/PX Selection Region')
+    # ax['B'].fill_between(-1.0*px,-1.0*pu,pu,fc='None',hatch=htch,alpha=alph,edgecolor=c,lw=lw,ls=ls)
+    # ax['B'].fill_between([xylim*physmult+1,xylim*physmult+2],[xylim*physmult+1,xylim*physmult+2],[xylim*physmult+3,xylim*physmult+4],fc='w',hatch='//',alpha=.5,edgecolor=c,lw=lw,ls=ls,label='B/PX Selection Regions')
+
+    # if barlims[tind]['barmax']>barlims[tind]['barmid']:
+    #     circle1 = Circle((0,0),barlims[tind]['barmid']*physmult,facecolor='None',edgecolor='cyan',ls='-',label='Bar Peak')
+    #     ax['B'].add_patch(circle1)
+    #     circle2 = Circle((0,0),barlims[tind]['barmax']*physmult,facecolor='None',edgecolor='limegreen',ls='-.',label='Bar Outer Limit')
+    #     ax['B'].add_patch(circle2)
+    #     ax['B'].legend(prop={'size':10},loc=2)
+    # # cts,xb,yb,img=ax['B'].hist2d(xaposatframe[x],xaposatframe[y],bins=(bbx,bby),alpha=0.1,norm=mpl.colors.LogNorm(),cmap='binary')
+
+    ax['B'].set_xlim((-1*xylim-xycen)*physmult,(xylim-xycen)*physmult)
+    ax['B'].set_ylim((-1*xylim-xycen)*physmult,(xylim-xycen)*physmult)
+    ax['B'].set_ylabel(y+pustr)
+    ax['B'].set_xlabel(x+pustr)
+    ax['B'].grid(lw=.5,c='grey',alpha=.5)
+    ax['B'].set_title('%s, tind %i'%(simstr,tind),size=24)
+    if sv:
+        plt.savefig('plots/'+simstr+'/%i_simovertime.png'%tind,dpi=300,bbox_inches='tight')
+        plt.close()
+    else:
+        plt.show()
