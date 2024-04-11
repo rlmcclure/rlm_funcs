be8390d23b74ad4b11c7c1d8a9a6d6069625886d
Rachel <58311515+rlmcclure@users.noreply.github.com>
2024-04-11 16:22:38 -0400

    add histthehex and double plot and double scatter plot plotters

--- plotters.py
+++ plotters.py
@@ -19,6 +19,21 @@ params = {'axes.labelsize': 20,
           'ytick.labelsize': 16}
 plt.rcParams.update(params)
 
+def wbkg(fs=None,rtax=0,alph=1,fc='white'):
+    '''give a figure a white background'''
+    if rtax:
+        f,ax = plt.subplots(figsize=fs,tight_layout=True)
+    else:
+
+        f = plt.figure(figsize=fs,tight_layout=True)
+
+    f.patch.set_facecolor(fc)
+    f.patch.set_alpha(alph)
+    if rtax:
+        return(f,ax)
+    else:
+        return f
+
 #%%
 def quick_imshow(twod_field,field_title=None,cmapp='viridis',cmin=None,cmax=None,rtnfig=0):
     '''
@@ -124,6 +139,7 @@ def quick_hist(data, field_title = None,nbins=100,rng=None,nstd=1,rtnfig=0):
         return(fig)
     else:
         plt.show()
+
         #%% pixel by pixel plot from eleanor.Visualization tess package
 def pixel_by_pixel(box, colrange=None, rowrange=None, cmap='BrBG', mask=None,
                     xlim=None,ylim=None, color_by_pixel=True, 
@@ -250,21 +266,6 @@ def linplot(fig,ax,m,b,x,ylims,label,ls='--',alpha=.8,c='grey',lw=3,lbl=1):
     return(fig,ax)
 
 
-def wbkg(fs=None,rtax=0,alph=1,fc='white'):
-    '''give a figure a white background'''
-    if rtax:
-        f,ax = plt.subplots(figsize=fs,tight_layout=True)
-    else:
-
-        f = plt.figure(figsize=fs,tight_layout=True)
-
-    f.patch.set_facecolor(fc)
-    f.patch.set_alpha(alph)
-    if rtax:
-        return(f,ax)
-    else:
-        return f
-
 
 def simovert(frame,simstr='',nestdir='plots/',xylim=20,xycen=0,zlim=5,zcen=0,sv=1,alph=.7,lw=.5,c='k',htch=None,ls='--',cmapp='magma_r',physmult=1,pustr='',binsn=2000,tind=None):
     '''
@@ -331,3 +332,205 @@ def simovert(frame,simstr='',nestdir='plots/',xylim=20,xycen=0,zlim=5,zcen=0,sv=
         plt.close()
     else:
         plt.show()
+
+
+def doubleplot(x,y1,y2,structarr=None,c1='darkred',s2=':',c2='darkgreen',title='quick plot',fs=(9,6)):
+    f = wbkg(fs)
+    ax = f.add_subplot()
+    if structarr is not None:
+        ax.plot(structarr[x],structarr[y1],c=c1)
+        ax.set_ylabel(y1)
+        ax.set_xlabel(x)
+        ax2 = ax.twinx()
+        ax2.set_ylabel(y2)
+        ax2.plot(structarr[x],structarr[y2],c=c2,ls=s2)
+        if title == 'quick plot':
+            try:
+                plt.suptitle(namestr(structarr),fontsize=14,y=.85)
+            except IndexError:
+                plt.suptitle(title,fontsize=14,y=.85)
+    else:
+        ax.plot(x,y1,c=c1)
+        try:
+            ax.set_ylabel(namestr(y1))
+            ax.set_xlabel(namestr(x))
+        except IndexError:
+            ax.set_ylabel('')
+            ax.set_xlabel('')
+        ax2 = ax.twinx()
+        try:
+            ax2.set_ylabel(namestr(y2))
+        except IndexError:
+            ax2.set_ylabel('')
+        ax2.plot(x,y2,c=c2,ls=s2)
+        plt.suptitle(title,fontsize=14,y=.85)
+    ax.yaxis.label.set_color(c1)
+    ax.tick_params(axis='y', colors=c1)
+    ax2.yaxis.label.set_color(c2)
+    ax2.tick_params(axis='y', colors=c2)
+    ax.grid(c=c1,alpha=.4)
+    ax2.grid(ls=':',c=c2,alpha=.3)
+    plt.show()
+
+def doublescplot(x,y1,c1,y2,c2,x2=None,title='quick plot',k1=None,k2=None,kx=None,kc1=None,kc2=None,cm1='inferno',ct1='darkred',cm2='cividis',ct2='darkblue',fs=(11,5),vlines=None,tratio=1):
+    f = wbkg(fs)
+    ax = f.add_subplot()
+    if x2 is None:
+        x2 = np.copy(x)
+        
+    #set title label strings
+    try:
+        if kx is None:
+            kx = namestr(x)
+        if k1 is None:
+            k1 = namestr(y1)
+        if kc1 is None:
+            k1 = namestr(c1)
+        if k2 is None:
+            k2 = namestr(y2)
+        if kc2 is None:
+            kc2 = namestr(c2)
+    except IndexError:
+        kx = '';k1 = '';k2 = ''
+
+    #plots
+    o1 = ax.scatter(x,y1,c=c1,cmap=cm1,marker='o',alpha=.7)
+    if vlines is not None:
+        ax.axvline(x=vlines[0],ls='--',c='k',alpha=.5)
+        ax.axvline(x=vlines[1],ls='--',c='k',alpha=.5)
+    ax.set_ylabel(k1,fontsize=12)
+    ax.set_xlabel(kx,fontsize=12)
+    ax2 = ax.twinx()
+    ax2.set_ylabel(k2,fontsize=12)
+    o2 = ax2.scatter(x2,y2,c=c2,cmap=cm2,marker='D')
+
+    # add ratio through interpolating
+    if tratio == True:
+
+        #colorbars, has to padding adjusted for extra colorbar ofc
+        cb = f.colorbar(o1,pad=-.05)#%total')#.1)
+        cb.set_label(label=kc1,size=12,c=ct1)
+        cb.ax.tick_params(labelsize=10,color=ct1)
+        cb = f.colorbar(o2,pad=-.05)#%total')#.1)
+        cb.set_label(label=kc2,size=12,c=ct2)
+        cb.ax.tick_params(labelsize=10,color=ct2)
+
+        tvals = np.linspace(0,SIM.tlensim*2,len(SIM.times)) #hard coded cause i'm tired    
+        fn1 = interpolate.interp1d(x,c1,kind='nearest',fill_value='extrapolate')
+        fn2 = interpolate.interp1d(x2,c2,kind='nearest',fill_value='extrapolate')
+        o3 = ax2.scatter(tvals,np.zeros(len(tvals)),c=fn2(tvals)/fn1(tvals),s=30,marker='|',cmap='Paired_r')#,vmin=0.25,vmax=4.25)
+        cb = f.colorbar(o3,pad=.12)
+        cb.set_label(label='ratio of times, %s/%s'%(k2,k1),size=12,c=ct2)
+        cb.ax.tick_params(labelsize=10,color=ct2)
+
+    else:
+        #colorbars
+        cb = f.colorbar(o1,pad=-.02)#%total')#.1)
+        cb.set_label(label=kc1,size=12,c=ct1)
+        cb.ax.tick_params(labelsize=10,color=ct1)
+        cb = f.colorbar(o2,pad=.12)#%total')#.1)
+        cb.set_label(label=kc2,size=12,c=ct2)
+        cb.ax.tick_params(labelsize=10,color=ct2)
+
+    #grids
+    ax.tick_params(axis='x',labelsize=10)
+    ax.yaxis.label.set_color(ct1)
+    ax.tick_params(axis='y', colors=ct1,labelsize=10)
+    ax2.yaxis.label.set_color(ct2)
+    ax2.tick_params(axis='y', colors=ct2,labelsize=10)
+    ax.grid(c=ct1,alpha=.4)
+    ax2.grid(ls=':',c=ct2)
+        
+    plt.suptitle(title,fontsize=14,y=.9)
+    plt.show()
+
+
+def histhexplt(plttype,xvals,yvals,cvals=None,hexfn=np.median,f=None,ax=None,fs=None,ptitle='Quick Plot',xlim=(None,None),ylim=(None,None),vlim=(None,None),xbins=None,ybins=None,gsxdiv=100,gsydiv=100,logtog=0,densitytog=0,cmapp='viridis',xlabel=None,ylabel=None):
+    if f is None:
+        f,ax = wbkg(fs,rtax=1)
+    elif ax is None:
+        ax = f.add_subplot()
+
+    #set gridsize from the values being plotted, use gridsize dividers (gsydiv, gsxdiv) to set size dynamcially
+    if ybins is None:
+        if gsydiv is not None:
+            ybins = int(np.floor(len(np.unique(yvals))/gsydiv))
+        else: 
+            ybins = int(len(np.unique(yvals)))
+    if xbins is None:
+        if gsxdiv is not None:
+            xbins = int(np.floor(len(np.unique(xvals))/gsxdiv))
+        else: 
+            xbins = int(len(np.unique(xvals)))
+    gs_bs = (xbins,ybins) #number of bins in x and y
+
+    #set extent and range by xlim and ylim if given, otherwise it is set by inar params later
+    if xlim[0] is None:
+        xv = xvals[np.isfinite(xvals)]
+        xmin = np.amin(np.unique(xv))
+    else:
+        xmin = xlim[0]
+    if xlim[1] is None:
+        xv = xvals[np.isfinite(xvals)]
+        xmax = np.amax(np.unique(xv))
+    else:
+        xmax = xlim[1]
+    
+    if ylim[0] is None:
+        yv = yvals[np.isfinite(yvals)]
+        ymin = np.amin(np.unique(yv))
+    else:
+        ymin = ylim[0]
+    if ylim[1] is None:
+        yv = yvals[np.isfinite(yvals)]
+        ymax = np.amax(np.unique(yv))
+    else:
+        ymax = ylim[1]
+    ex_rg = ([xmin,xmax],[ymin,ymax])
+
+    if plttype == 'hist':
+        if logtog == 1:
+            normtog = mpl.colors.LogNorm()
+        else:
+            normtog=None
+        if densitytog == 1:## are these indexings are wrong
+            todump=ax.hist2d(xvals.flatten(),yvals.flatten(),bins=gs_bs,range=ex_rg,norm=normtog,vmin=vlim[0],vmax=vlim[1],cmap=cmapp)
+            
+            ax.pcolormesh(todump[1], todump[2], (todump[0]/np.sum(todump[0])).T,vmin=vlim[0],vmax=vlim[1],cmap=cmapp)        
+        else:
+            histout=ax.hist2d(xvals.flatten(),yvals.flatten(),bins=gs_bs,range=ex_rg,norm=normtog,vmin=vlim[0],vmax=vlim[1],cmap=cmapp)
+
+        if densitytog == 1:
+            f.colorbar(histout,ax=ax)#,label='relative density')
+        else:
+            f.colorbar(histout[3],ax=ax)#,label='N')
+
+    elif plttype == 'hex':
+        if logtog == 1:
+            binstog ='log'
+        else:
+            binstog=None
+        hexout=ax.hexbin(xvals,yvals,C=cvals,fn=hexfn,gridsize=gs_bs,bins=binstog,extent=list(np.array(ex_rg).flat),vmin=vlim[0],vmax=vlim[1],cmap=cmapp)
+        if densitytog == 1:
+            outar = hexout.get_array()
+            hexout.set_array(outar/np.sum(outar))
+            if vlim[0] is None:
+                hexout.set_clim(np.amin(hexout.get_array()),np.amax(hexout.get_array()))
+            else:
+                hexout.set_clim(vlim[0],vlim[1])
+        if densitytog == 1:
+            f.colorbar(hexout,ax=ax)#,label='relative density')
+        # else:
+            # f.colorbar(hexout,ax=ax)#,label='N')
+
+    ax.set_xlim(xmin,xmax)
+    ax.set_ylim(ymin,ymax)
+    # ax.set_xticklabels([])
+    # ax.set_yticklabels([])
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    ax.grid(linestyle=':',linewidth=1)
+    ax.set_title(ptitle)
+
+    return(f,ax)
