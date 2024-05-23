bcb8663eef3ce5a3eec0a632c56e94c2db627653
Rachel Lee McClure <rlmcclure@wisc.edu>
2024-05-23 13:30:58 -0500

    added overlay contours

--- plotters.py
+++ plotters.py
@@ -34,6 +34,8 @@ myprettycs = json.loads('["#001b00","#001a00","#001900","#001800","#001700","#00
 mypretty2cs = json.loads('["#001b00","#002100","#002600","#002b00","#003000","#003400","#003800","#003b00","#003e00","#004100","#004300","#004500","#004700","#004800","#004a00","#004b00","#004c00","#004c00","#004d00","#004d00","#004d00","#024d00","#094d00","#114d00","#184c00","#204c00","#274b00","#2f4b00","#374a00","#3f4900","#474900","#4f4800","#574800","#5f4700","#674600","#6e4600","#764500","#7d4500","#844400","#8b4402","#92440e","#984319","#9e4324","#a4432f","#a9433a","#ae4444","#b3444f","#b7445a","#bb4564","#bf466e","#c24778","#c54882","#c7498c","#c94a95","#cb4b9e","#cc4da7","#cd4fb0","#cd50b8","#cd53c0","#cc55c8","#cb57d0","#ca59d7","#c85cde","#c65fe5","#c362eb","#c065f2","#bd68f7","#b96bfd","#b56fff","#b173ff","#ac76ff","#a67aff","#a17eff","#9b83ff","#9587ff","#8f8bff","#8890ff","#8194ff","#7a99ff","#739eff","#6ba3ff","#63a8ff","#5cadff","#54b2ff","#4cb7ff","#44bcff","#3cc1ff","#34c6ff","#2cccff","#25d1ff","#1dd6ff","#16dbff","#0ee0ff","#07e5ff","#01eaff","#00efff","#00f4ff","#00f9ff","#00fdff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00ffff","#00fffe","#00fffc","#00fffa","#00fff8","#00fff6","#00fff4","#0cfff3","#18fff2","#26fff1","#36fff1","#47fff1","#5afff1","#6ffff2","#86fff3","#9efff4","#b9fff6","#d5fff9","#f4fffc"]')
 mypretty3cs = json.loads('["#001b00","#001a00","#091a00","#121900","#1b1900","#231900","#2b1900","#321900","#391900","#401a00","#461a00","#4c1b00","#521b00","#571c00","#5c1d00","#601e00","#641f00","#682000","#6c2100","#6f2200","#722400","#752500","#772700","#792800","#7b2a00","#7c2c00","#7d2d00","#7e2f00","#7f3100","#7f3300","#803500","#803700","#803900","#7f3c00","#7f3e00","#7e4000","#7d4300","#7c4500","#7a4800","#794a00","#774d00","#754f07","#73520e","#715515","#6f581c","#6d5a23","#6b5d2a","#686031","#656338","#63663f","#606946","#5d6c4d","#5a6f54","#57725a","#547561","#517868","#4e7b6f","#4b7e75","#48817c","#458483","#428789","#3e8a90","#3b8d96","#38909c","#3593a2","#3297a9","#2f9aaf","#2c9db5","#29a0bb","#26a3c0","#24a6c6","#21a9cc","#1eacd1","#1cafd7","#1ab2dc","#17b5e1","#15b8e6","#13bbeb","#11bef0","#0fc1f5","#0ec3f9","#0cc6fe","#0bc9ff","#0accff","#09ceff","#08d1ff","#08d4ff","#07d6ff","#07d9ff","#07dbff","#07ddff","#08e0ff","#09e2ff","#0ae4ff","#0be7ff","#0ce9ff","#0eebff","#10edff","#13efff","#15f1ff","#18f2ff","#1bf4ff","#1ff6ff","#23f7ff","#27f9ff","#2cfaff","#30fcff","#36fdff","#3bfeff","#41ffff","#48ffff","#4effff","#55ffff","#5dffff","#65ffff","#6dffff","#76ffff","#7fffff","#89ffff","#93ffff","#9dffff","#a8ffff","#b4ffff","#c0ffff","#ccffff","#d9ffff","#e6ffff","#f4fffc"]')
 lavhaze = json.loads('["#37002d","#380031","#3a0035","#3b0039","#3d003d","#3f0041","#400044","#420047","#44004b","#46004d","#470050","#490153","#4b0355","#4d0557","#4f0759","#50095b","#520b5d","#540d5e","#560f60","#581161","#5a1462","#5c1663","#5d1864","#5f1a65","#611c65","#631e66","#652166","#672367","#692567","#6b2767","#6d2967","#6f2c67","#712e67","#733067","#753267","#763567","#783766","#7a3966","#7c3b66","#7e3e65","#804065","#824264","#844564","#864763","#884963","#894c62","#8b4e62","#8d5061","#8f5361","#915560","#925760","#94595f","#965c5f","#985e5e","#99605e","#9b635e","#9d655d","#9f685d","#a06a5d","#a26c5d","#a36f5d","#a5715d","#a7735d","#a8765d","#aa785d","#ab7a5d","#ac7d5e","#ae7f5e","#af815f","#b1845f","#b28660","#b38861","#b58b62","#b68d64","#b78f65","#b89166","#b99468","#ba966a","#bb986c","#bc9b6e","#bd9d70","#be9f72","#bfa275","#c0a478","#c1a67b","#c2a87e","#c2ab81","#c3ad85","#c4af88","#c4b18c","#c5b490","#c5b695","#c6b899","#c6ba9e","#c7bca3","#c7bfa8","#c7c1ae","#c7c3b3","#c8c5b9","#c8c7c0","#c8c9c6","#c8cccd","#c8ced4","#c7d0db","#c7d2e3","#c7d4eb","#c7d6f3","#c6d8fb","#c6daff","#c5dcff","#c5deff","#c4e0ff","#c4e2ff","#c3e4ff","#c2e6ff","#c1e8ff","#c0eaff","#bfecff","#beeeff","#bdf0ff","#bcf2ff","#baf4ff","#b9f5ff","#b8f7ff","#b6f9ff","#b5fbff","#b3fdff","#b1feff"]')
+candy = json.loads('["#00355e","#003560","#003461","#003363","#003365","#003267","#003269","#00316b","#00316d","#00306f","#003070","#003072","#003074","#002f76","#002f77","#002f79","#032f7b","#0c2f7c","#142f7e","#1c2f80","#242f81","#2c3083","#343084","#3b3086","#433087","#4a3189","#51318a","#58328c","#5f328d","#66338f","#6c3390","#723491","#793493","#7f3594","#853695","#8b3797","#903898","#963999","#9b399a","#a13a9c","#a63b9d","#ab3d9e","#af3e9f","#b43fa0","#b940a1","#bd41a2","#c143a3","#c544a4","#c945a5","#cd47a6","#d148a7","#d44aa8","#d84ba9","#db4daa","#de4fab","#e150ac","#e452ad","#e654ae","#e956ae","#eb58af","#ee59b0","#f05bb1","#f25db2","#f360b2","#f562b3","#f764b4","#f866b4","#f968b5","#fa6bb5","#fb6db6","#fc6fb7","#fd72b7","#fd74b8","#fd77b8","#fe79b9","#fe7cb9","#fe7eba","#fd81ba","#fd84ba","#fd87bb","#fc89bb","#fb8cbc","#fa8fbc","#f992bc","#f895bc","#f698bd","#f59bbd","#f39ebd","#f1a2bd","#f0a5be","#eda8be","#ebabbe","#e9afbe","#e6b2be","#e4b6be","#e1b9be","#debdbe","#dbc0be","#d8c4be","#d4c8be","#d1cbbe","#cdcfbe","#c9d3be","#c5d7be","#c1dbbe","#bddfbe","#b8e3be","#b4e7bd","#afebbd","#aaefbd","#a5f3bd","#a0f7bd","#9bfbbc","#96ffbc","#90ffbc","#8affbb","#85ffbb","#7fffba","#79ffba","#72ffba","#6cffb9","#65ffb9","#5fffb8","#58ffb8","#51ffb7","#4affb7","#42ffb6","#3bffb6"]')
+
 def cmapsegmenter(strorclist,nseg=256):
     try:
         if type(strorclist) == str:
@@ -44,6 +46,25 @@ def cmapsegmenter(strorclist,nseg=256):
         print('Issue somewhere, trouble shoot for now just giving back input.')
         cmapp = strorclist
     return(cmapp)
+def registercmap(cmaplist,nseg=256,r=0):
+    if type(cc) == str:
+        if cc[-2:]=='_r':
+            plt.register_cmap(cmap=mpl.cm.get_cmap(cc,lut=nseg))
+        elif r:
+            plt.register_cmap(cmap=mpl.cm.get_cmap(cc+'_r',lut=nseg))
+        else:
+            plt.register_cmap(cmap=mpl.cm.get_cmap(cc,lut=nseg))
+
+        
+    elif type(cc) == list:
+        if r:
+            plt.register_cmap(cmap=LinearSegmentedColormap.from_list(namestr(cmaplist)+'_r', cmaplist[::-1],nseg))
+        else:
+            plt.register_cmap(cmap=LinearSegmentedColormap.from_list(namestr(cmaplist), cmaplist,nseg))
+ns=256
+for cc in [myspringcs, myfunnyvalentinecs, myprettycs, mypretty2cs, mypretty3cs, lavhaze, mymosscs, myterrecs, candy, 'cividis', 'viridis', 'plasma', 'magma']:
+    registercmap(cc,nseg=ns)
+    registercmap(cc,nseg=ns,r=1)
 
 def wbkg(fs=None,rtax=0,alph=1,fc='white'):
     '''give a figure a white background'''
@@ -292,6 +313,33 @@ def linplot(fig,ax,m,b,x,ylims,label,ls='--',alpha=.8,c='grey',lw=3,lbl=1):
     return(fig,ax)
 
 
+def getfromout_hist(out):
+    zs = out[0]
+    xs = out[1]
+    ys = out[2]
+    return(xs,ys,zs)
+  
+def getfromout_hex(out):
+    zs = out.get_array()
+    xs = out.get_offsets().T[0]
+    ys = out.get_offsets().T[1]
+    return(xs,ys,zs)
+
+def overlaycontour(f,ax,out,c='k',lvls=5,lw=2,rtarr=0,hex=True,hist=False):
+    if hex:
+        xs,ys,zs = getfromout_hex(out)
+    elif hist:
+        xs,ys,zs = getfromout_hist(out)
+        
+    try:
+        ax.tricontour(xs,ys,zs,levels=lvls,colors=c,linewidths=lw)
+    except ValueError:
+        ax.tricontour(xs,ys,zs,levels=lvls,cmap=c,linewidths=lw)
+    if rtarr:
+        return(f,ax,out)
+    else:
+        return(f,ax)
+
 
 def simovert(frame,simstr='',nestdir='plots/',xylim=20,xycen=0,zlim=5,zcen=0,sv=1,alph=.7,lw=.5,c='k',htch=None,ls='--',cmapp='magma_r',physmult=1,pustr='',binsn=2000,tind=None):
     '''
@@ -470,7 +518,7 @@ def doublescplot(x,y1,c1,y2,c2,x2=None,title='quick plot',k1=None,k2=None,kx=Non
     plt.show()
 
 
-def histhexplt(plttype,xvals,yvals,cvals=None,hexfn=np.mean,f=None,ax=None,fs=None,ptitle='Quick Plot',xlim=(None,None),ylim=(None,None),vlim=(None,None),xbins=None,ybins=None,gsxdiv=100,gsydiv=100,logtog=0,densitytog=0,cmapp='viridis',xlabel=None,ylabel=None,rtax=0):
+def histhexplt(plttype,xvals,yvals,cvals=None,hexfn=np.sum,f=None,ax=None,fs=None,ptitle='Quick Plot',xlim=(None,None),ylim=(None,None),vlim=(None,None),xbins=None,ybins=None,gsxdiv=100,gsydiv=100,logtog=0,densitytog=0,cmapp='viridis',xlabel=None,ylabel=None,rtax=0):
     if f is None:
         f,ax = wbkg(fs,rtax=1)
     elif ax is None:
